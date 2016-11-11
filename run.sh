a62099955147c48ace3a4718d7e76a58e4e1dfea
Mohamad Nasr-Azadani <mmnasr@gmail.com>
2016-11-11 13:16:31 -0800

    Changed extension of input files from csv to txt.

--- run.sh
+++ run.sh
@@ -9,5 +9,5 @@ echo "Comiling ./src/digitalwallet.cpp using c++11 compiler\n"
 rm *.o runcode
 g++ -std=c++11 -O3 -ftree-vectorize ./src/digitalwallet.cpp -o ./runcode 
 echo "Runnig code ...\n"
-./runcode ./paymo_input/batch_payment.csv ./paymo_input/stream_payment.csv ./paymo_output/
+./runcode ./paymo_input/batch_payment.txt ./paymo_input/stream_payment.txt ./paymo_output/
 
